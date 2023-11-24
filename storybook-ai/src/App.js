6883a6a09789dc848411c126de197c12eed65612
Arisbeth Aguirre <a01274803@itesm.mx>
2023-11-24 17:41:50 +0900

    [WEB] Added Delete Modal

--- storybook-ai/src/App.js
+++ storybook-ai/src/App.js
@@ -1,33 +1,66 @@
+import { useState } from "react";
 import "./App.css";
 import AddCard from "./components/AddCard";
+import ConfirmationModal from "./components/ConfirmationModal";
 import NewCard from "./components/NewCard";
+import { FaTrash } from "react-icons/fa";
 
 function App() {
+
+  const [showDelete, setShowDelete] = useState(false)
+
+  function handleOpenDelete() {
+    setShowDelete(true);
+  }
+
+  const handleCloseDelete = () => {
+    setShowDelete(false)
+  }
+
   return (
-    <div className="bg-amber-100 min-h-screen w-full space-y-10 py-10 items-center justify-center">
-      <div className="self-start font-extralight text-6xl ml-10">Bookshelf</div>
-      <div className="flex flex-row flex-wrap">
-        <AddCard />
-        <NewCard
-          color={"bg-orange-200"}
-          title={"Harry Potter and the Philosopher’s Stone"}
-        />
-        <NewCard
-          color={"bg-pink-200"}
-          title={"A Hitchhiker’s Guide to the Galaxy"}
-        />
-        <NewCard color={"bg-blue-200"} title="Nineteen eighty-four" />
-        <NewCard
-          color={"bg-orange-200"}
-          title={"Harry Potter and the Philosopher’s Stone"}
-        />
-        <NewCard
-          color={"bg-pink-200"}
-          title={"A Hitchhiker’s Guide to the Galaxy"}
-        />
-        <NewCard color={"bg-blue-200"} title={"Nineteen eighty-four"} />
+    <>
+      <div className="bg-amber-100 min-h-screen w-full space-y-10 py-10 items-center justify-center">
+        <div className="flex flex-row flex-wrap place-content-between align-middle">
+          <div className="self-start font-extralight text-6xl ml-10">
+            Bookshelf
+          </div>
+          <button className="rounded-lg hover:brightness-75 bg-red-400 text-white font-medium text-xl mr-40 px-3" onClick={handleOpenDelete}>
+            <div className="flex flex-row place-content-center">
+              <FaTrash className="mr-3 mt-1" /> Delete a book
+            </div>
+          </button>
+        </div>
+
+        <div className="flex flex-row flex-wrap">
+          <AddCard />
+          <NewCard
+            color={"bg-orange-200"}
+            title={"Harry Potter and the Philosopher’s Stone"}
+          />
+          <NewCard
+            color={"bg-pink-200"}
+            title={"A Hitchhiker’s Guide to the Galaxy"}
+          />
+          <NewCard color={"bg-blue-200"} title="Nineteen eighty-four" />
+          <NewCard
+            color={"bg-orange-200"}
+            title={"Harry Potter and the Philosopher’s Stone"}
+          />
+          <NewCard
+            color={"bg-pink-200"}
+            title={"A Hitchhiker’s Guide to the Galaxy"}
+          />
+          <NewCard color={"bg-blue-200"} title={"Nineteen eighty-four"} />
+        </div>
       </div>
-    </div>
+
+      <ConfirmationModal
+        show={showDelete}
+        onClose={handleCloseDelete}
+        text={"Do you wish to delete this book?"}
+        method={() => console.log('Deleted')}
+      />
+    </>
   );
 }
 
--- storybook-ai/src/components/ConfirmationModal.js
+++ storybook-ai/src/components/ConfirmationModal.js
@@ -6,20 +6,20 @@ import { HiOutlineExclamationCircle } from 'react-icons/hi'
 const ConfirmationModal = ({ show, onClose, text, method }) => {
   const content = (
     <>
-      <Modal show={show} size='md' popup onClose={onClose} dismissible>
+      <Modal show={show} size='md' popup onClose={onClose} dismissible className='bg-white p-5'>
         <ModalHeader />
         <ModalBody>
           <div className='text-center'>
             <HiOutlineExclamationCircle className='mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200' />
-            <h3 className='mb-5 text-lg font-normal text-gray-500 dark:text-gray-400'>
+            <h3 className='mb-5 text-lg font-normal'>
               {text}
             </h3>
             <div className='flex justify-center gap-4'>
-              <Button color='failure' onClick={method}>
-                Confirmar
+              <Button className='!bg-red-500 hover:brightness-75' onClick={method}>
+                Confirm
               </Button>
-              <Button className='!bg-gray-500' onClick={onClose}>
-                Cancelar
+              <Button className='!bg-gray-500 hover:brightness-75' onClick={onClose}>
+                Cancel
               </Button>
             </div>
           </div>
